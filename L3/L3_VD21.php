06777d4669e3bb987d1592339b0111a4327467b0
Truong Nguyen <165233624+Jamejsskjdad@users.noreply.github.com>
2026-01-28 08:29:54 +0700

    Update vi du ham nac danh

--- L3/L3_VD21.php
+++ L3/L3_VD21.php
@@ -1,29 +1,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Ví dụ về hàm</title>
+    <title>Ví dụ về hàm hồi qui</title>
 </head>
 <body>
 <?php
-    // Truyền tham số kiểu tham chiếu
-    function add_some_extra(&$string){
-        $string .= ' sau khi chịu tác động của hàm.';
-    }
+    // Sử dụng hàm nặc danh làm hàm hồi qui 
+    echo preg_replace_callback('~-([a-z])~', function ($match) {
+        return strtoupper($match[1]);
+    }, 'hello-world');
+    // outputs helloWorld
 
-    $str = 'Chuỗi ban đầu';
-    echo "$str<br>";
-    add_some_extra($str);
-    echo $str;
+    // Sử dụng hàm nặc danh làm giá trị của một biến
+    $greet = function ($name) {
+        printf("Hello %s\r\n", $name);
+    };
 
-    // Hàm có sử dụng tham số với giá trị ngầm định
-    echo "<br>Ví dụ về hàm sử dụng tham số với giá trị ngầm định<br>";
-    function makecoffee($type = "cappuccino"){
-        return "Making a cup of $type<br>";
-    }
-
-    echo makecoffee();
-    echo makecoffee(null);
-    echo makecoffee("espresso");
+    $greet('World');
+    $greet('PHP');
 ?>
 </body>
 </html>
